c5fd4e6e67daaf3428e31afcbd246f72ed9afdf8
Richard Douglas <r.douglas.42092@gmail.com>
2023-07-01 00:40:09 -0700

    come on, baby girl....

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors())
 app.use(express.static(path.join(__dirname, 'build')))
 app.get('*', (req, res) => {
 
-    res.sendFile(path.join(__dirname+'/build/index.js'));
+    res.sendFile(path.join(__dirname+'/src/index.js'));
   })
 // app.use(
 //     session({
